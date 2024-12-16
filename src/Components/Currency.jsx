706161a323f75f47d4350d47c1711cc7a64b04f4
Ziyoviddin880 <ziyoviddinabdurashidov7@gmail.com>
2024-12-16 13:36:44 +0500

    Change Api Method

--- src/Components/Currency.jsx
+++ src/Components/Currency.jsx
@@ -1,31 +1,33 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Currency = () => {
-   const API_KEY = "deb73e0c60-4b33e10ffd-sokr93";
-   const [finalResult, setFinalResult] = useState([]);
+  const API_KEY = "deb73e0c60-4b33e10ffd-sokr93";
+  const [finalResult, setFinalResult] = useState([]);
 
-   axios({
+  useEffect(() => {
+    axios({
       method: "GET",
       url: `https://api.fastforex.io/currencies?api_key=${API_KEY}`,
-   })
-      .then((response) => response.json())
-      .then((data) => setFinalResult(data))
-      .then(console.log(finalResult));
+    }).then((response) => {
+      console.log(response.data.currencies);
+      setFinalResult(response.data.currencies);
+    });
+  }, []);
 
-   return (
+  return (
+    <div>
+      <h1>Currency</h1>
       <div>
-         <h1>Currency</h1>
-         <div>
-            <select>
-               {finalResult.map((item) => (
-                  <option key={item} value={item}>
-                     {item}
-                  </option>
-               ))}
-            </select>
-         </div>
+        <select>
+          {Object.entries(finalResult).map(([key, value]) => (
+            <option key={key} value={key}>
+              {key.toUpperCase()} :{value}
+            </option>
+          ))}
+        </select>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Currency;
