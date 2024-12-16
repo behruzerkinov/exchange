5c12caa049c86e715c3c64605c232426494510bb
Behruz Erkinov <erkinovbehruz0@gmail.com>
2024-12-16 12:49:27 +0500

    axios usage

--- src/Components/Currency.jsx
+++ src/Components/Currency.jsx
@@ -1,11 +1,28 @@
-import Row from "./Row";
-
+import axios from "axios";
+import { useState } from "react";
 const Currency = () => {
+   const API_KEY = "deb73e0c60-4b33e10ffd-sokr93";
+   const [finalResult, setFinalResult] = useState([]);
+
+   axios({
+      method: "GET",
+      url: `https://api.fastforex.io/currencies?api_key=${API_KEY}`,
+   })
+      .then((response) => response.json())
+      .then((data) => setFinalResult(data))
+      .then(console.log(finalResult));
+
    return (
       <div>
          <h1>Currency</h1>
          <div>
-            <Row />
+            <select>
+               {finalResult.map((item) => (
+                  <option key={item} value={item}>
+                     {item}
+                  </option>
+               ))}
+            </select>
          </div>
       </div>
    );
